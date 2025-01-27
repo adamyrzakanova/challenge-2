e5531c4278141d54db48514c3c135ec58853a631
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-01-26 22:12:54 -0500

    Task 1 - Store Inventory.

---
+++ cc_2.js
@@ -0,0 +1,8 @@
+// task 1
+let products = ["necklace", "earrings", "bracelet", "ring", "pendant"];
+products.push("brooch");
+console.log(products);
+products.pop(); 
+console.log("Updated products:",products);
+
+
