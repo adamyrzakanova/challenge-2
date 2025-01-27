9f914a3e86a49adc1637d775abc4a9d209a808a3
adamyrzakanova <aidaimyrzakanova@usf.edu>
2025-01-26 22:15:25 -0500

    Task 2 - Student Scores.

--- cc_2.js
+++ cc_2.js
@@ -5,4 +5,13 @@ console.log(products);
 products.pop(); 
 console.log("Updated products:",products);
 
+// task 2
+let scores = [85, 90, 78, 92, 88];
+scores[1] = 95;
+let total = scores.reduce((a, b) => a + b, 0);
+let average = total / scores.length;
+console.log("Updated scores:", scores);
+console.log("Total score:", total);
+console.log("Average score:", average);
+
 
